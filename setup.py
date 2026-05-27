a5e0150f490a3b5804d620ba2e520659292b718d
Dan Fornika <dfornika@gmail.com>
2026-05-27 14:15:00 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='auto-illumina-run-qc-check',
-    version='0.1.0-alpha',
+    version='0.1.0',
     packages=find_namespace_packages(),
     entry_points={
         "console_scripts": [
