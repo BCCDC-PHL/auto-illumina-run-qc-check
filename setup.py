f932c9e60b3c02a038b03696b2710658d55bc47d
Dan Fornika <dfornika@gmail.com>
2026-06-15 14:33:00 -0700

    Bump version from 0.1.0 to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='auto-illumina-run-qc-check',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_namespace_packages(),
     entry_points={
         "console_scripts": [
